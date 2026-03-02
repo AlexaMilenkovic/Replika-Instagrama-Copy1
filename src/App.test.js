df3b7279e9ae16189f312aa17ff0dc70201a7d8b
AlexaMilenkovic <150688558+AlexaMilenkovic@users.noreply.github.com>
2026-03-02 15:21:28 +0100

    Update test to check for welcome message
    Ovaj i prethodni commit su tu kako bi ispravio greske od proslog puta, i kako bi kod mogao da stigne do build i push faze za docker.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderuje početnu poruku', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/dobrodošli/i);
+  expect(element).toBeInTheDocument();
 });
