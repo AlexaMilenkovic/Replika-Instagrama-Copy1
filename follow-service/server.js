facdbb49c3cd088f8ea87192f9a190979138066b
anaprogramiranje <ana.marija.urukalo@gmail.com>
2026-03-06 11:45:05 +0100

    added additional functionalities and changes in implementation

--- follow-service/server.js
+++ follow-service/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const db = require('./config/db');
 
+// Uvozimo kontroler 
+const FollowController = require('./controllers/FollowController');
+
 const app = express();
 app.use(express.json()); 
 
@@ -17,19 +20,19 @@ app.get('/test-db', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Follow servis radi na portu ${PORT}`);
-});
+// 2. Rute za Praćenje (Follow) i Blokiranje (Block) 
+app.post('/follow', FollowController.followUser);             // Slanje zahteva
+app.put('/follow/accept', FollowController.acceptFollow);      // Prihvatanje zahteva
+app.delete('/follow/reject', FollowController.rejectFollow);   // Odbijanje zahteva
+app.get('/follow/notifications/:userId', FollowController.getNotifications); // Lista zahteva
+app.delete('/followers/remove', FollowController.removeFollower); // uklanjanje pratioca
+app.get('/block-status', FollowController.getBlockStatus);        //blok status
+app.get('/relationship-status', FollowController.getRelationshipStatus);  //follow status
+app.delete('/unfollow', FollowController.unfollowUser);       // Prekid praćenja
+app.post('/block', FollowController.blockUser);               // Blokiranje
+app.get('/stats/:userId', FollowController.getStats);         // Statistika
 
-// Rute za Praćenje (Follow) i Blokiranje (Block) 
-app.post('/follow', FollowController.followUser);             // Slanje zahteva (Javni/Privatni)
-app.put('/follow/accept', FollowController.acceptFollow);      // Prihvatanje zahteva 
-app.delete('/follow/reject', FollowController.rejectFollow);   // Odbijanje zahteva 
-app.get('/follow/notifications/:userId', FollowController.getNotifications); // Lista zahteva za meni
 
-app.delete('/unfollow', FollowController.unfollowUser);       // Prekid praćenja 
-app.post('/block', FollowController.blockUser);               // Blokiranje 
-app.get('/stats/:userId', FollowController.getStats);         // Broj pratilaca i praćenih 
 app.listen(PORT, () => {
   console.log(`Follow servis radi na portu ${PORT}`);
 });
