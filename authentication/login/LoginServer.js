2ae2a3d7d1db0eb56a9066e99bd0adf4bd519745
AlexaMilenkovic <150688558+AlexaMilenkovic@users.noreply.github.com>
2026-03-06 13:12:19 +0100

    Delete authentication/login directory

--- authentication/login/LoginServer.js
+++
@@ -1,5 +0,0 @@
-const app = require('./LoginController');
-
-app.listen(3002, () => {
-  console.log('Login service running on port 3002');
-});
--- authentication/login/prijavaServe.js
+++
@@ -1,5 +0,0 @@
-const app = require("./prijava");
-
-app.listen(3002, () => {
-  console.log("Login service running on port 3002");
-});
