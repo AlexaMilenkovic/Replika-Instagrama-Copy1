fa34b88a8fa363aa5df83339a1dabcb7c4662b7f
AlexaMilenkovic <150688558+AlexaMilenkovic@users.noreply.github.com>
2026-03-06 13:12:30 +0100

    Delete authentication/logout directory

--- authentication/logout/LogoutServer.js
+++
@@ -1,5 +0,0 @@
-const app = require('./LogoutController');
-
-app.listen(3003, () => {
-  console.log('Logout service running on port 3003');
-});
--- authentication/logout/odjavaServe.js
+++
@@ -1,5 +0,0 @@
-const app = require("./odjava");
-
-app.listen(3003, () => {
-  console.log("Logout service running");
-});
