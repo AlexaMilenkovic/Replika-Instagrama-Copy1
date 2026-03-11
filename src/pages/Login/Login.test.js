a6ada5fc3236f36f24be4d6d1a1dbabbd17341a1
Ksenija Živković <ksenijai670@gmail.com>
2026-03-11 19:11:56 +0100

    add unit tests for Profile, Navbar, and Login components

--- src/pages/Login/Login.test.js
+++ src/pages/Login/Login.test.js
@@ -1,15 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Login from './Login';
 
-test('proverava da li Login stranica sadrži polja za kredencijale', () => {
-  render(
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
-  );
-  
-  // Provera za kredenacijale (zahtev 1.1.1)
-  expect(screen.getByPlaceholderText(/Korisničko ime ili email/i)).toBeInTheDocument();
-  expect(screen.getByPlaceholderText(/Lozinka/i)).toBeInTheDocument();
+// da li dugme stvarno preusmerava
+const mockedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedNavigate,
+}));
+
+describe('Login Komponenta', () => {
+
+  test('proverava da li Login stranica sadrži polja za kredencijale i dugme', () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+    
+    expect(screen.getByPlaceholderText(/Korisničko ime ili email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Lozinka/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Prijavi se/i })).toBeInTheDocument();
+  });
+
+  // Test kucanje i kliktanje 
+  test('omogućava unos teksta u polja i klik na dugme za prijavu', () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const identifierInput = screen.getByPlaceholderText(/Korisničko ime ili email/i);
+    const passwordInput = screen.getByPlaceholderText(/Lozinka/i);
+    const submitButton = screen.getByRole('button', { name: /Prijavi se/i });
+
+    // kucanje korisnika u input polja
+    fireEvent.change(identifierInput, { target: { value: 'ksenija_dev' } });
+    fireEvent.change(passwordInput, { target: { value: 'tajnalozinka123' } });
+
+    expect(identifierInput.value).toBe('ksenija_dev');
+    expect(passwordInput.value).toBe('tajnalozinka123');
+
+    fireEvent.click(submitButton);
+
+    expect(mockedNavigate).toHaveBeenCalledWith('/');
+  });
 });
