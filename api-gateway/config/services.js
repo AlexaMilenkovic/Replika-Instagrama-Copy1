9f63431dc52146344baf4b6a937d2d05b6e691c3
anaprogramiranje <ana.marija.urukalo@gmail.com>
2026-03-13 22:23:45 +0100

    refactor of follow-service and api gateway implementation

---
+++ api-gateway/config/services.js
@@ -0,0 +1,8 @@
+module.exports = {
+  authentication: 'http://authentication_service:3001',
+  follow: 'http://follow-service:3004',
+  profile: 'http://profile-service:3013',
+  feed: 'http://feed:3015',
+  interactions: 'http://interactions-service:3002',
+  post: 'http://post-service:3001'
+};
